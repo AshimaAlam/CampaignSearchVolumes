51f5c311458f57e3a9c9fe4ab5f6890177423845
AshimaAlam <69263543+AshimaAlam@users.noreply.github.com>
2020-08-07 13:16:48 +0200

    Final Updates

--- app.R
+++ app.R
@@ -13,34 +13,63 @@ data$Rn = as.integer(data$Media.Spend)
 data
 
 ui <- fluidPage(
-    titlePanel("Campaign Search Volumes"),
+  titlePanel("Campaign Search Volumes"),
   
-      sidebarLayout(
-      sidebarPanel(
-        selectInput(inputId = "MediaCampaign",label = "Media Campaign", choices =  c(data$MediaCampaign), selected = "data$MediaCampaign", multiple = F),
-        sliderInput(inputId = "RetentionFactor", label = "Retention Factor ", min = 1,max = 10, value= 1,step = 0.1),
-            ),
-      
-      mainPanel(
-        plotOutput(outputId = "distPlot"),
-        verbatimTextOutput(outputId ="stats"),
-      )
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "MediaCampaign",label = "Media Campaign", choices =  c(data$MediaCampaign), selected = "data$MediaCampaign", multiple = F),
+      sliderInput(inputId = "RetentionFactor", label = "Retention Factor ", min = 1,max = 10, value= 1,step = 0.1),),
+    
+    mainPanel(
+      plotOutput(outputId = "distPlot"),
+      tableOutput("TbEfficiencies"),
     )
   )
+)
 
 server <- function(input, output) {
-    
-      selectedData <- reactive({
-        data %>% filter(data$MediaCampaign==input$MediaCampaign)
-      })
-      
-      output$distPlot <- renderPlot({
-        plot(x=selectedData()$index,y=selectedData()$Media.Spend, xlab="week",ylab="Adstock", type="h")
-      })
-      
-      output$stats <- renderPrint({
-        summary(selectedData()$Media.Spend)
-      })
+  
+  selectedData <- reactive({
+    data %>% filter(data$MediaCampaign==input$MediaCampaign)
+  })
+  
+  
+  output$distPlot <- renderPlot({
+    for (i in 1:nrow(data))
+    {
+      j <- lag(i, n = 1L, default = NA, order_by = NULL)
+      PrvWeekMediaSpend <- data[j,2]
     }
+    
+    ###Adstock (in week n) = Media Spend (in week n) + [ RF x Adstock (in week n-1) ]
+    #RF <- input$RetentionFactor * data()$PrvAd
+    #cbind(data, RF)
+    #Adstock <- data()$Media.Spend + data()$RF
+    
+    RF = selectedData()$Media.Spend + (input$RetentionFactor * selectedData()$Media.Spend)
+    plot(x=selectedData()$index,y=RF, xlab="Week",ylab="Adstock", type="h")
+  })
+  
+  Values <- reactive({
+    
+    data.frame(
+      Name = c("Highest Search Volume",
+               "Lowest Search Volume",
+               "Average Search Volume"),
+      Value = as.character(c(paste(max(selectedData()$Media.Spend)),
+                             paste(min(selectedData()$Media.Spend)),
+                             paste(mean(selectedData()$Media.Spend))
+      )),
+      
+      stringsAsFactors = FALSE)
+    
+  })
+  
+  output$TbEfficiencies <- renderTable({
+    Values()
+  })    
+  
+}
+
 
 shinyApp(ui = ui, server = server)
